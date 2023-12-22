c27d4ff5d64802874cbe0d8a145163b8a54e4a3c
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-12-22 14:35:05 +0100

    board.h 90%

---
+++ include/Board.h
@@ -0,0 +1,30 @@
+//@Barbato alberto 2073961
+
+#ifndef BOARD_H
+#define BOARD_H
+#include "Tile.h"
+#include "Position.h"
+#include <array>
+class Board{
+    const static int BOARD_SIZE = 32;
+
+    Position get_partenza()const;
+
+    const Tile& get_tile(Position) const;
+    Tile& get_tile(Position);
+
+    void set_tile(const Tile&, const Position&);
+
+    bool avanza_e_controlla(Position&,int) const;//retsituisce true se si passa dal via
+
+    bool is_valid_position(Position) const;
+
+    //da espandere se serve
+private:
+    std::array<Tile,BOARD_SIZE> m_tiles;
+};
+
+std::ostream operator<<(std::ostream&, const Board&);
+
+
+#endif
