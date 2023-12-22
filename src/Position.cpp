da8da5354f5dcc736f9a1c521d02d054e5cb6ab3
elborgosuco <126857349+elborgosuco@users.noreply.github.com>
2023-12-22 15:25:01 +0100

    Position.cpp

--- src/Position.cpp
+++ src/Position.cpp
@@ -4,19 +4,23 @@
 #include "../include/Board.h"
 std::ostream& operator<<(std::ostream& os,const Position& pos)
 {
+	const int line_size = (Board::BOARD_SIZE / 4)+1; // 8
+	const int column_size = (Board::BOARD_SIZE / 4)-1; // 6
 	std::string outp;
-	if (pos.get_valore() < ) {
+	if (pos.get_valore() < line_size) { // 0 - 7
 		outp = "H" + (8 - pos.get_valore());
 	}
-	else if (pos.get_valore() < 16) {
-		int temp_val = pos.get_valore() - 8; // 0 - 7
-		outp = static_cast<char>('H' - temp_val)+ "" + (temp_val + 1);
+	else if (pos.get_valore() < line_size + column_size) { // 8 - 13
+		int temp_val = pos.get_valore() - line_size; // 0 - 7
+		outp = static_cast<char>('G' - temp_val)+ "1";
 	}
-	else if (pos.get_valore() < 24) {
-		int temp_val = pos.get_valore() - 16; // 0 - 7
+	else if (pos.get_valore() < line_size*2 + column_size) {  //16 - 21
+		int temp_val = pos.get_valore() - (line_size + column_size); // 0 - 7
+		outp = "A" + (temp_val + 1);
 	}
-	else
+	else // 21 - 28
 	{
-		int temp_val = pos.get_valore() - 32; // 0 - 7
+		int temp_val = pos.get_valore() - (2*line_size + column_size);; // 0 - 7
+		outp = const_cast<char>('B' + temp_val) + "8";
 	}
 }
