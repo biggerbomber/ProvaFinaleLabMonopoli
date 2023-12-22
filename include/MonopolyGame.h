81d125d1f7f53291834c01e96ecb8dfb9b4aa269
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-12-22 19:24:56 +0100

    tile.cpp e qualche fix

--- include/MonopolyGame.h
+++ include/MonopolyGame.h
@@ -4,22 +4,29 @@
 #define MONOPOLYGAME_H
 #include "Board.h"
 #include "Player.h"
+#include "Tile.h"
 #include <iostream>
+#include <memory>
+#include <fstream>
 
 class MonopolyGame{
 public:
-    const static int N_PLAYER=4;
-    enum PlayerType {BOT ,HUMAN};
+  const static int N_PLAYER=4;
+  enum PlayerType {BOT ,HUMAN};
+  enum PlayerTag{ALPHA, BETA, GAMMA,DELTA};
 
-    MonopolyGame(PlayerType);
-    void run();
+  MonopolyGame(PlayerType);
+  void run();
 private:
-    Board m_board;
-    std::array<Player*,N_PLAYER> m_players;
-    //Aggiungere i/o
+  Board m_board;
+  std::array<std::unique_ptr<Player>,N_PLAYER> m_players;
+  std::array<int, 4> m_num_turno;
+  std::ofstream m_output_file;
 };
 
 std::ostream& operator<<(std::ostream&, const MonopolyGame&);
+std::ostream& operator<<(std::ostream&, const MonopolyGame::PlayerTag&);
 
+int roll_dice();
 
 #endif
--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,1 +1,67 @@
-//Tile.cpp
+//@Barbato Alberto 2073961
+#include "../include/Tile.h"
+
+Tile::Tile(Tile::TileType tt) {
+  m_tile_type = tt;
+  m_build_type = Tile::BuildType::VUOTA;
+  m_posseduta = false;
+}
+
+int Tile::get_proprietario() const{
+  if (has_proprietario()) {
+    return m_proprietario;
+  }
+  throw std::invalid_argument("La Tile non ha un proprietario");
+}
+int Tile::get_costo_terreno() const {
+  std::array<int, Tile::N_TILE_TYPE> costo_terreno = { 0,6,10,20 };
+  return costo_terreno[m_tile_type];
+}
+int Tile::get_costo_pernottamento() const {
+  std::array<std::array<int, Tile::N_BUILD_TYPE>, Tile::N_TILE_TYPE> costo_pernottamento
+  { { //vuota,casa,albergo
+      {0,0,0}, // terreno angolare
+      {0,2,4}, // economico
+      {0,4,8}, //standard
+      {0,7,14} //lusso
+  } }; 
+  return costo_pernottamento[m_tile_type][m_build_type];
+}
+
+int Tile::get_costo_miglioramento() const {
+  std::array<std::array<int, Tile::N_BUILD_TYPE>, Tile::N_TILE_TYPE> costo_miglioramneto
+  { { //vuota,casa,albergo
+      {0,0,0}, // terreno angolare
+      {3,3,0}, // economico
+      {5,5,0}, //standard
+      {10,10,0} //lusso
+  } };
+  return costo_miglioramneto[m_tile_type][m_build_type];
+}
+
+std::ostream& operator<<(std::ostream& os, const Tile& tile) {
+  if (tile.has_proprietario()) {
+    os << "|" << tile.get_tile_type() << tile.get_build_type() << tile.get_proprietario() << "|";
+
+  }
+  else {
+    os << "|" << tile.get_tile_type() << tile.get_build_type() << "|";
+
+  }
+  return os;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Tile::TileType& t_type) {
+
+  std::array<char, Tile::N_TILE_TYPE> tiletype_charset = { ' ','E','S','L' };
+  return os << tiletype_charset[t_type];
+};
+
+std::ostream& operator<<(std::ostream& os, const Tile::BuildType& b_type) {
+  if (b_type == Tile::BuildType::VUOTA) {
+    return os;
+  }
+  std::array<char, Tile::N_BUILD_TYPE> buildtype_charset = {'?','*','^'};
+  return os << buildtype_charset[b_type];
+};
