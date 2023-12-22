d643f3d657b3978725f6e45449b1aa73c8abf35a
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-12-22 21:28:11 +0100

    tester tile non esaustivo

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,12 @@ int main(int argc , char** argv){
     Tile t(Tile::ANGOLARE);
 
     std::cout << t;
-
-
+    t.set_tile_type(Tile::ECONOMICA);
+    t.set_build_type(Tile::ALBERGO);
+    t.set_proprietario(3);
+    std::cout << t;
+    t.set_build_type(Tile::CASA);
+    t.set_tile_type(Tile::LUSSO);
+    std::cout << t;
 
 }
