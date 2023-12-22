1199adf7d877c4ff8e13a560c7b66c0e63a7b30c
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-12-22 14:02:42 +0100

    Finito tile.h (finorma)

--- include/Tile.h
+++ include/Tile.h
@@ -1,24 +1,44 @@
 //@Barbato Alberto 2073961
 
 
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef TILE_H
+#define TILE_H
 #include "Player.h"
-#include <vector>
+#include <array>
+#include <iostream>
 
 class Tile{
 public:
+    const static int N_TILE_TYPE=4;
     enum TileType { ANGOLARE, ECONOMICA, STANDARD, LUSSO};
+    const static int N_BUILD_TYPE=3;
     enum BuildType { VUOTA, CASA, ALBERGO };
 
-    const static std::vector<int> s_costo_tereno;
+    const static std::array<int,N_TILE_TYPE> s_costo_tereno;
+    const static std::array<std::array<int,N_TILE_TYPE>,N_BUILD_TYPE> s_costo_miglioramento;
+    const static std::array<std::array<int,N_TILE_TYPE>,N_BUILD_TYPE> s_costo_pernottamento;
 
-    int a;
-    Tile();
+
+    Tile(TileType);
+
+    TileType get_tile_type() const;
+    BuildType get_build_type() const;
+    //Enum player get
+
+    void set_tile_type(TileType);
+    void set_build_type(BuildType);
+     //Enum player set
+private:
+    TileType m_tile_type;
+    BuildType m_build_type;
+    //Da aggiungere referendce all enum di monopli game PROPRIETARIO
 
 };
 
 
+std::ostream& operator<<(std::ostream&,const Tile&);
+std::ostream& operator<<(std::ostream&,const Tile::TileType&);
+std::ostream& operator<<(std::ostream&,const Tile::BuildType&);
 
 
 
--- include/test.cpp
+++
@@ -1,3 +0,0 @@
-#include "Tile.h"
-const  std::vector<int> Tile::s_costo_tereno =  {0,6,10,20};
-
