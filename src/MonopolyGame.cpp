ae2782634b566a511f7297897e82de120e88ad8e
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-12-23 17:46:39 +0100

    ddd

--- src/MonopolyGame.cpp
+++ src/MonopolyGame.cpp
@@ -1,5 +1,8 @@
 //monopolygame.cpp
 #include "../include/MonopolyGame.h"
+#include "../include/Bot.h"
 
-
-
+std::shared_ptr<Player> MonopolyGame::get_player_from_tag(int)
+{
+  return std::shared_ptr<Player>(new Bot(0,0,0));
+}
